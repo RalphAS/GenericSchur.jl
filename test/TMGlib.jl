9fc905f52b06da95eb788b0aeee3fb7c1d433960
RalphAS <smith.ralph.a@gmail.com>
2021-01-19 19:09:03 -0500

    improvements to test library interface
    This change improves documentation and implements some extra options
    in the wrappers of LAPACK test generation routines.
    It also adds the extra character-length arguments needed for recently
    compiled versions of the library.

--- test/TMGlib.jl
+++ test/TMGlib.jl
@@ -15,28 +15,41 @@ const latm_modes = Dict(:specified => 0, :clustered_small => 1,
                     :clustered_large => 2, :exponential => 3,
                     :arithmetic => 4, :random_log => 5, :random => 6)
 
+function _check_iseed(iseed::Vector{T}) where {T<:Integer}
+    if length(iseed) < 4
+        throw(ArgumentError("iseed must have (at least) 4 elements"))
+    end
+    if any(iseed .< 0) || any(iseed .> 4095)
+        throw(ArgumentError("iseed entries must be in 0:4095"))
+    end
+    isodd(iseed[4]) || throw(ArgumentError("iseed[4] must be odd"))
+    nothing
+end
+
 """
 construct a random non-symmetric square matrix with specified eigenvalues
 
-`latme!(A::Matrix{Complex{T}}, anorm::Real, imode::Int, rcond::Real,
-        simrcond::Real = 1.0;
-        iseed::Vector{BlasInt} = [2518, 3899, 995, 397],
-        kl::Int = size(A,2), ku::Int = size(A,2),
-        dist::AbstractChar = 'D', rsign::Bool = true,
-        upper::Bool = true, simtrans::Bool = true,
-        simmode::Int = 4)`
+`latme!(A::Matrix{<:BlasFloat}, anorm::Real, imode::Int, rcond::Real,
+simrcond::Real = 1.0;
+iseed::Vector{BlasInt} = [2518, 3899, 995, 397],
+kl::Int = size(A,2), ku::Int = size(A,2),
+dist::AbstractChar = 'D', rsign::Bool = true,
+upper::Bool = true, simtrans::Bool = true,
+simmode::Int = 4, d::Vector, ds::Vector)`
 
-`imode`: 1 - clustered small, 2 - clustered large, 3 - exponential,
-       4 - arithmetic, 5 - random log, 6 - random (<0 for reversed order)
-`imode = 0`, explicitly specified eigenvalues, is not yet implemented.
-`rcond`: ratio of extreme eigenvalues
-`kl,ku`: lower and upper bandwidths
+- `imode`: 1 - clustered small, 2 - clustered large, 3 - exponential,
+     4 - arithmetic, 5 - random log, 6 - random (<0 for reversed order),
+     0 - eigenvalues explicitly specified in `d`.
+- `rcond`: ratio of extreme eigenvalue magnitudes.
+- `anorm`: if positive, finally scale the matrix so its maximum-element-norm is `anorm`.
+- `kl,ku`: lower and upper bandwidths
+- If `upper`, fill the upper triangle (make result non-normal).
+- If `rsign`, eigenvalues have random signs (real) or angles (complex).
+- If `simtrans`, apply a similarity transform with singular values
+      specified by `simmode, simrcond, ds`, similarly to `imode, rcond, d`.
 
-If `simtrans == 'T'` apply a similarity transform with singular values
-specified by `simmode, simrcond`, similarly to `imode,rcond`.
-
-`dist`: character key for distribution to be sampled: U - uniform(0,1),
-   S - symmetric uniform(-1,1), N - normal, D - uniform on complex unit disk.
+- `dist`: character key for distribution to be sampled: `'U'` - uniform(0,1),
+   `'S'` - symmetric uniform(-1,1), `'N'` - normal, `'D'` - uniform on complex unit disk.
 """
 function latme! end
 
@@ -65,6 +78,7 @@ for (fname, elty, relty) in ((:zlatme_, :ComplexF64, :Float64),
             end
             (dist ∈ ['D','U','S','N']) || throw(ArgumentError("allowed values of dist are 'D','U','S', and 'N'"))
 
+            _check_iseed(iseed)
             work = Vector{$elty}(undef, 3*n)
             info = Ref{BlasInt}(0)
 
@@ -88,11 +102,12 @@ for (fname, elty, relty) in ((:zlatme_, :ComplexF64, :Float64),
                    Ptr{$elty}, # A
                    Ref{BlasInt}, # lda
                    Ptr{$elty}, # work(w)
-                   Ptr{BlasInt} # info(w)
+                   Ptr{BlasInt}, # info(w)
+                   Clong, Clong, Clong, Clong # char lengths
                    ),
                   n, dist, iseed, d, imode, rcond, dmax,
                   rsign ? 'T' : 'F', upper ? 'T' : 'F', simtrans ? 'T' : 'F',
-                  ds, simmode, simrcond, kl, ku, anorm, A, lda, work, info)
+                  ds, simmode, simrcond, kl, ku, anorm, A, lda, work, info, 1,1,1,1)
             (info[] == 0) || error("info = $(info[]) from latme!")
             A, d, ds, iseed
         end
@@ -111,13 +126,16 @@ for (fname, celty, elty) in ((:dlatme_, :ComplexF64, :Float64),
                 upper::Bool = true, simtrans::Bool = true,
                 simmode::Int = 4,
                 dmax::$elty = one($elty),
-                d::Vector{$elty} = Vector{$elty}(undef,size(A,2)),
+                d = nothing,
                 ds::Vector{$elty} = Vector{$elty}(undef,size(A,2))
                 )
             n = size(A,2)
             lda = size(A,1)
 
+            d1 = Vector{$elty}(undef,size(A,2))
+
             if imode == 0
+                (d isa Vector) || throw(ArgumentError("imode = 0 needs vector d"))
                 (length(d) == n) || throw(ArgumentError("imode = 0 needs explicit d of length n"))
                 if simtrans
                     (length(ds) == n) || throw(ArgumentError("imode = 0 needs explicit ds of length n"))
@@ -125,6 +143,7 @@ for (fname, celty, elty) in ((:dlatme_, :ComplexF64, :Float64),
             end
             (dist ∈ ['U','S','N']) || throw(ArgumentError("allowed values of dist are 'U','S', and 'N'"))
 
+            _check_iseed(iseed)
             work = Vector{$elty}(undef, 3*n)
             info = Ref{BlasInt}(0)
 
@@ -143,7 +162,33 @@ for (fname, celty, elty) in ((:dlatme_, :ComplexF64, :Float64),
 #            If MODE is not 0, then EI is ignored.  If MODE is 0 and
 #            EI(1)=' ', then the eigenvalues will all be real.
 
-            ei = ' '
+            if imode == 0
+                if eltype(d) <: Real
+                    ei = UInt8[' ']
+                    d1 .= d[1:n]
+                else
+                    ei = Vector{UInt8}(undef, n)
+                    ie = 1
+                    id = 1
+                    while id <= n
+                        ei[ie] = 'R'
+                        d1[ie] = real(d[id])
+                        ie += 1
+                        if imag(d[id]) != 0
+                            if d[id+1] != conj(d[id])
+                                throw(ArgumentError("conjugate pairs in d must be adjacent"))
+                            end
+                            ei[ie] = 'I'
+                            d1[ie] = imag(d[id])
+                            ie += 1
+                            id += 1
+                        end
+                        id += 1
+                    end
+                end
+            else
+                ei = UInt8[' ']
+            end
 
             ccall((@blasfunc($fname), liblapack), Cvoid,
                   (Ref{BlasInt}, # n
@@ -166,13 +211,15 @@ for (fname, celty, elty) in ((:dlatme_, :ComplexF64, :Float64),
                    Ptr{$elty}, # A
                    Ref{BlasInt}, # lda
                    Ptr{$elty}, # work(w)
-                   Ptr{BlasInt} # info(w)
+                   Ptr{BlasInt}, # info(w)
+                   Clong, Clong, Clong, Clong, Clong # char lengths
                    ),
-                  n, dist, iseed, d, imode, rcond, dmax, ei,
+                  n, dist, iseed, d1, imode, rcond, dmax, ei,
                   rsign ? 'T' : 'F', upper ? 'T' : 'F', simtrans ? 'T' : 'F',
-                  ds, simmode, simrcond, kl, ku, anorm, A, lda, work, info)
+                  ds, simmode, simrcond, kl, ku, anorm, A, lda, work, info,
+                  1,1,1,1,1)
             (info[] == 0) || error("info = $(info[]) from latme!")
-            A, d, ds, iseed
+            A, d1, ds, iseed
         end
     end
 end
@@ -192,15 +239,15 @@ latmr!(A::Matrix{T<:BLAStype}, anorm::Real, imode::Int, rcond::Real,
                 pack::AbstractChar = 'N'
                 )
 
-`imode`: 1 - clustered small, 2 - clustered large, 3 - exponential,
+- `imode`: 1 - clustered small, 2 - clustered large, 3 - exponential,
        4 - arithmetic, 5 - random log, 6 - random (<0 for reversed order)
-`imode = 0`, explicitly specified eigenvalues, is not yet implemented.
-`sym`: `'S'` - symmetric, `'H'` - Hermitian, `'N'` - nonsymmetric
-`rcond`: ratio of extreme eigenvalues
-`kl,ku`: lower and upper bandwidths
-`sparse`: fraction to be nulled out
-`dist`: character key for distribution to be sampled: U - uniform(0,1),
-   S - symmetric uniform(-1,1), N - normal, D - uniform on complex unit disk.
+- `imode = 0`, explicitly specified diagonal, is not yet implemented.
+- `sym`: `'S'` - symmetric, `'H'` - Hermitian, `'N'` - nonsymmetric
+- `rcond`: ratio of extreme diagonal elements
+- `kl,ku`: lower and upper bandwidths
+- `sparse`: fraction to be nulled out
+- `dist`: character key for distribution to be sampled: `'U'` - uniform(0,1),
+   `'S'` - symmetric uniform(-1,1), `'N'` - normal, `'D'` - uniform on complex unit disk.
 """
 function latmr! end
 
@@ -231,6 +278,7 @@ for (fname, elty, relty) in ((:zlatmr_, :ComplexF64, :Float64),
             m = lda # CHECKME: is it worth generalizing?
 
             (imode == 0) && throw(ArgumentError("imode = 0 needs explicit d,ds; not yet implemented"))
+            _check_iseed(iseed)
 
             iwork = Vector{BlasInt}(undef, max(m,n))
             info = Ref{BlasInt}(0)
@@ -271,13 +319,15 @@ for (fname, elty, relty) in ((:zlatmr_, :ComplexF64, :Float64),
                    Ptr{$elty}, # A
                    Ref{BlasInt}, # lda
                    Ptr{BlasInt}, # iwork(w)
-                   Ptr{BlasInt} # info(w)
+                   Ptr{BlasInt}, # info(w)
+                   Clong, Clong, Clong, Clong, Clong, Clong
                    ),
                   m, n, dist, iseed, sym, d, imode, rcond, dmax,
                   rsign ? 'T' : 'F', grade, dl, model, rcondl,
                   dr, moder, rcondr, pivtng, ipivot,
-                  kl, ku, sparse, anorm, pack, A, lda, iwork, info)
-#            (info[] == 0) || error("info = $(info[]) from latmr")
+                  kl, ku, sparse, anorm, pack, A, lda, iwork, info,
+                  1,1,1,1,1,1)
+            (info[] == 0) || error("info = $(info[]) from latmr")
             A, d, dl, dr, iseed
         end
     end
